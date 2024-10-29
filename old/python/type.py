4d62d5f536ddb1fef5e8f14b198b1984c4f324b0
Meby123B <meby123b@gmail.com>
2024-10-29 23:45:46 +0200

    using pynput insted of pyautogui

--- old/python/type.py
+++ old/python/type.py
@@ -1,7 +1,15 @@
 import sys
-import pyautogui
+# import pyautogui
 
-letter = sys.argv[1] 
+# letter = sys.argv[1] 
 
 # https://github.com/gandalf113/node-python-tutorial
-pyautogui.typewrite(letter)
+# pyautogui.typewrite(letter)
+
+
+from pynput.keyboard import Controller, Key
+keyboard = Controller()
+
+
+keyboard.press('ש')
+keyboard.release('ש')
